fa9e9fe4a745bd7ff7bc50043a5029e64a166bed
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-09 13:42:34 -0500

    Exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+range = (1..100)
+
+range.each do |num|
+if (num % 3 == 0) && (num % 5 == 0)
+	puts "BitMaker"
+elsif (num % 3 == 0)
+	puts "Bit"
+elsif (num % 5 == 0)
+	puts "Maker" 
+else 
+	puts num
+end
+end
