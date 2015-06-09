f95c50032db4733954f3269cb6f089e810852cf2
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-09 12:59:53 -0500

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+puts 55 * 1.15
+
+puts "hello" + 1.to_s
+
+randommath = 45628 * 7839
+puts "hello here is some random math: #{randommath}"
+
+puts (true & false) || (false && true) || !(false && false)
